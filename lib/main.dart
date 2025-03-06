46bec7f178cb032d34973a172d54caa3cfcb6896
sowndharya <sowndharya@zed.digital>
2025-03-06 17:08:44 +0530

    initial push

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:demouserapp/utils.dart';
 import 'package:demouserapp/view/dripple/drip_ui_01.dart';
 
 void main() {
+  //initial push1
   runApp(
     MultiProvider(
       providers: [
