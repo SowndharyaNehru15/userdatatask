2cba0852a3b903fec4377365f4431a0dcefe7099
sowndharya <sowndharya@zed.digital>
2025-03-06 17:54:50 +0530

    Updated 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,15 @@ import 'package:demouserapp/utils.dart';
 import 'package:demouserapp/view/dripple/drip_ui_01.dart';
 
 void main() {
-  //github test
+  //github test 2
   //bitbucket
+  
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => UserViewModel()..fetchUsers()),
       ],
-      
+
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home:
